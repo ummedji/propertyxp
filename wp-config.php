57c4bc46d086aad276380f3ace8968d6e616398c
ummedji <ummedsingh.sekhawat@webcluesglobal.com>
2016-04-20 17:47:09 +0530

    adding config file changes

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-echo $_SERVER["HTTP_HOST"];die;
+
 if($_SERVER["HTTP_HOST"] == "www.propertyxp.com"){
 
 define('DB_NAME', 'property_proxp_local');
