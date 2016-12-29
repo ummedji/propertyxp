032ee81b3b0d16e45f1b7e624b9474068bf1c701
webcluesinfotech <sales@webcluesinfotech.com>
2016-12-29 18:21:53 +0530

    adding changes

--- wp-content/themes/realocation/page-newsletter.php
+++ wp-content/themes/realocation/page-newsletter.php
@@ -15,7 +15,8 @@
 		    <div class="clearfix">
 		        <div class="col-md-12">
 		            <h2>
-		                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+		                <!--<a href="<?php /*the_permalink(); */?>"><?php /*the_title(); */?></a>-->
+						<?php the_title(); ?>
 		                <?php echo aviators_edit_post_link(); ?>
 		            </h2>
 		
