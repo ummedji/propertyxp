e304eacc59c371e9e7efda0413cb805eeeda77c8
webcluesinfotech <sales@webcluesinfotech.com>
2017-01-13 10:57:39 +0530

    adding changes

--- wp-content/themes/realocation/functions.php
+++ wp-content/themes/realocation/functions.php
@@ -1,9 +1,9 @@
 <?php
-
+/*
 add_action('init', 'do_output_buffer');
 function do_output_buffer() {
     ob_start();
-}
+}*/
 
 define ('AVIATORS_SIDEBARS_ALL', 1);
 define ('AVIATORS_SIDEBARS_ANY', 0);
